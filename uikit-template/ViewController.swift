247bae76163f8fd33f673b8be0dfc500bebff9c8
Loopy <loopy@heyratel.com>
2025-10-03 15:37:28 +0900

    init project

---
+++ uikit-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uikit-template
+//
+//  Created by 신동규 on 10/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red 
+    }
+
+
+}
+
